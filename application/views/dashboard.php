331a934e15d910b968f7f220626c7619a07c38ad
cael <cael@cael-linux.(none)>
2013-08-19 10:48:12 -0300

    estrutura com html5bp/ twitter boostrap / code igniter 2.1.4

---
+++ application/views/dashboard.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+
+        <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/css/normalize.css">
+        <!-- BOOTSTRAP -->
+        <!-- <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/bootstrap/css/bootstrap-responsive.css"> -->
+        <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/bootstrap/css/bootstrap.css">
+        <!-- BOOTSTRAP -->
+        <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/css/main.css">
+        <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/css/normalize.css">
+        <link rel="stylesheet" href="<?=$this->config->config['base_view']?>/css/style.css">
+
+        <script src="<?=$this->config->config['base_view']?>/js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+        
+        <!-- Add your site or application content here -->
+
+        <header>
+            
+            <h1>Logo do sistema grade</h1>
+            <nav>
+                <ul class="inline-block">
+                    <li><i class="icon-user"></i> <a href="#">Cadastre-se</a></li>
+                    <li><a href="#"></a>Logar</li>
+                </ul>
+            </nav>
+
+        </header>
+
+        <section>
+    
+            
+
+        </section>
+
+        <footer>
+            
+            <p class="text-right">Versão 0.1 Beta</p>
+        </footer>
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?=$this->config->config['base_view']?>js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="<?=$this->config->config['base_view']?>/js/plugins.js"></script>
+        <script src="<?=$this->config->config['base_view']?>/js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+        <script>
+            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+            g.src='//www.google-analytics.com/ga.js';
+            s.parentNode.insertBefore(g,s)}(document,'script'));
+        </script>
+    </body>
+</html>
